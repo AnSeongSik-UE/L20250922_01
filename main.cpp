273965376941221485d0d8a89c7d35aa157c444d
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-22 14:29:02 +0900

    함수화, 전역변수화
    함수화, 전역변수화

--- main.cpp
+++ main.cpp
@@ -5,77 +5,92 @@
 
 using namespace std;
 
+//LoadLevel
+int World[10][10] =
+{
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+bool bIsRunning = true;
+
+int PlayerX = 1;
+int PlayerY = 1;
+char PlayerShape = 'P';
+
+//사상(Mapping)
+char Sprites[10] = { ' ', '*', };
+
+int KeyCode;
+
+void Input()
+{
+	//Input
+	KeyCode = _getch();
+}
+
+void Process()
+{
+	//Process
+	if (KeyCode == 'w')
+	{
+		PlayerY--;
+	}
+	else if (KeyCode == 's')
+	{
+		PlayerY++;
+	}
+	else if (KeyCode == 'a')
+	{
+		PlayerX--;
+	}
+	else if (KeyCode == 'd')
+	{
+		PlayerX++;
+	}
+	else if (KeyCode == 'q')
+	{
+		bIsRunning = false;
+	}
+}
+
+void Render()
+{
+	//Render();
+	system("cls");
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << PlayerShape;
+			}
+			else
+			{
+				cout << Sprites[World[Y][X]];
+			}
+		}
+		cout << endl;
+	}
+}
+
 int main() 
 {
-	//LoadLevel
-	int World[10][10] =
-	{
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-	};
-
-	bool bIsRunning = true;
-
-	int PlayerX = 1;
-	int PlayerY = 1;
-	char PlayerShape = 'P';
-
-	//사상(Mapping)
-	char Sprites[10] = { ' ', '*', };
-
+	// Frame, DeltaSeconds(1/60)
 	while(bIsRunning)
 	{
-
-		//Input
-		int KeyCode = _getch();
-
-		//Process
-		if (KeyCode == 'w')
-		{
-			PlayerY--;
-		}
-		else if (KeyCode == 's')
-		{
-			PlayerY++;
-		}
-		else if (KeyCode == 'a')
-		{
-			PlayerX--;
-		}
-		else if (KeyCode == 'd')
-		{
-			PlayerX++;
-		}
-		else if (KeyCode == 'q')
-		{
-			bIsRunning = false;
-		}
-
-		//Render();
-		system("cls");
-		for (int Y = 0; Y < 10; ++Y)
-		{
-			for (int X = 0; X < 10; ++X)
-			{
-				if (PlayerX == X && PlayerY == Y)
-				{
-					cout << PlayerShape;
-				}
-				else
-				{
-					cout << Sprites[World[Y][X]];
-				}
-			}
-			cout << endl;
-		}
+		Input();
+		Process();
+		Render();
 	}
 
 	return 0;
